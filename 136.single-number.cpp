d356333f0e2366c0397406569f9ff5e6d4672fb6
Atharv Shingewar <shingewaratharv@gmail.com>
2021-12-14 15:58:30 +0530

    solved single number

---
+++ 136.single-number.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=136 lang=cpp
+ *
+ * [136] Single Number
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_set<int> hashset;
+        for(int i=0;i<nums.size();i++){
+            if(hashset.count(nums[i])>0)
+                hashset.erase(nums[i]);
+            else
+                hashset.insert(nums[i]);
+        }
+        
+        return *hashset.begin();
+    }
+};
+// @lc code=end
+
