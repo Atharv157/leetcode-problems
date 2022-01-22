1d83c05b1585edd2bf63fe0f7237fa97b445e849
Atharv Shingewar <shingewaratharv@gmail.com>
2022-01-22 16:55:50 +0530

    solved linked list cycle 2

--- 142.linked-list-cycle-ii.cpp
+++ 142.linked-list-cycle-ii.cpp
@@ -16,7 +16,35 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        ListNode *entry = head;
         
+        if(head==nullptr)
+            return nullptr;
+//         find the convergence point of slow and fast ptr
+        while(slow != nullptr && fast != nullptr && slow->next != nullptr && fast->next != nullptr)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+//             if convergence found then iterate both slow and entry till they match
+//             their matching point is the start of the cycle
+            if(slow == fast)
+            {
+                while(slow != entry)
+                {
+                    slow = slow->next;
+                    entry = entry->next;
+                }
+                if(slow == entry)
+                {
+                    return slow;
+                }
+                    
+            }
+        }
+        
+        return nullptr;
     }
 };
 // @lc code=end
