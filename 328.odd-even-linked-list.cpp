687b41b563ee6e29fe72d9520f9c67662106eb27
Atharv Shingewar <shingewaratharv@gmail.com>
2022-01-25 13:12:21 +0530

    added new solution for odd even linked list

--- 328.odd-even-linked-list.cpp
+++ 328.odd-even-linked-list.cpp
@@ -16,40 +16,61 @@
  * };
  */
 class Solution {
+    // better logic
 public:
-    ListNode* oddEvenList(ListNode* head) {
+     ListNode* oddEvenList(ListNode* head) {
+        if(!head || !head->next || !head->next->next) return head;
         
-        if(head == nullptr) return head;
+        ListNode *odd = head;
+        ListNode *even = head->next;
+        ListNode *even_start = head->next;
         
-        
-        ListNode *oddend = head;
-        ListNode *prev = head;
-        ListNode *curr  = prev->next;
-        
-        if(curr == nullptr) return head;
-        
-        int count = 2;
-        while(curr){
-            if(count%2 == 1)
-            {
-//                 odd node
-                prev->next = curr->next;
-                curr->next = oddend->next;
-                oddend->next = curr;
-                oddend = oddend->next;
-                curr = prev->next;
-            }
-            else
-            {
-//             even node
-                prev = prev->next;
-                curr = curr->next;
-            }
-            count++;
+        while(odd->next && even->next){
+            odd->next = even->next; //Connect all odds
+            even->next = odd->next->next;  //Connect all evens
+            odd = odd->next;
+            even = even->next;
         }
-        
-        return head;
+        odd->next = even_start;   //Place the first even node after the last odd node.
+        return head; 
     }
+
+
+    // my own previous logic
+// public:
+//     ListNode* oddEvenList(ListNode* head) {
+        
+//         if(head == nullptr) return head;
+        
+        
+//         ListNode *oddend = head;
+//         ListNode *prev = head;
+//         ListNode *curr  = prev->next;
+        
+//         if(curr == nullptr) return head;
+        
+//         int count = 2;
+//         while(curr){
+//             if(count%2 == 1)
+//             {
+// //                 odd node
+//                 prev->next = curr->next;
+//                 curr->next = oddend->next;
+//                 oddend->next = curr;
+//                 oddend = oddend->next;
+//                 curr = prev->next;
+//             }
+//             else
+//             {
+// //             even node
+//                 prev = prev->next;
+//                 curr = curr->next;
+//             }
+//             count++;
+//         }
+        
+//         return head;
+//     }
 };
 // @lc code=end
 
